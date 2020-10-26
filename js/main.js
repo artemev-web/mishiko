fbc72751edb23c0e3cb717d33cbada20a246fb49
Dmitry <72526120+artemev-web@users.noreply.github.com>
2020-10-26 12:31:31 +0300

    js new

--- js/main.js
+++ js/main.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
 									 })
 									 .always(function(response) {
 										//ссылка на страницу "спасибо" - редирект
-										location.href = 'thanks.html';
+										location.href = 'https://artemev-web.github.io/mishiko/thanks';
 								  });
 							 break;
                         break;
